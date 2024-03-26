e48be245a08de224ad62c5599323575e480c6038
Zander <Zandermcvey@gmail.com>
2024-03-26 15:09:13 -0500

    Animation WIP

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.AI;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+    private NavMeshAgent ai; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
